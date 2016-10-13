50c99aa13d5cbbe10db63e70de5c45433c2ff595
Nikita Shirokov <braindefender@gmail.com>
2016-10-13 14:33:30 +0700

    added "I use" and "html" sections

--- js/common.js
+++ js/common.js
@@ -1,5 +1,31 @@
 $(function() {
 
-	// Custom JS
+	var owl = $(".slider").owlCarousel({
+    loop: true,
+    nav: false,
+    items: 5,
+    autoplay: true,
+    responsiveClass: true,
+    responsive:{
+        0:{
+          items:3
+        },
+        525:{
+          items:5
+        },
+        768:{
+          items:3
+        },
+        910:{
+          items:5
+        },
+        1200:{
+          items:3
+        },
+        1410:{
+          items:5
+        }
+    }
+  });
 
 });
